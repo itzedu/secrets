770e7579a2a65b876b8161c15036a4b93adbf7b8
Eduardo <edubaik@gmail.com>
2015-07-19 23:52:36 -0700

    Created secret and like model. Only tested secret

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :secrets
+  has_many :likes, dependent: :destroy
+  has_many :secrets_liked, through: :likes, source: :secret
 
   email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
 
