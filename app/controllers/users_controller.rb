eed76a49eea55919f1b1f2e5b56a366f8397ef02
Eduardo <edubaik@gmail.com>
2015-07-19 17:20:39 -0700

    Added log in and log out with tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
+  def show
+    @user = User.find(params[:id])
   end
+  
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
 end
