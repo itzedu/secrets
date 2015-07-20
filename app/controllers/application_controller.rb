37a6a4e855c7f3006eaeb71d4a7a9e070ee7e16e
Eduardo <edubaik@gmail.com>
2015-07-20 16:49:39 -0700

    User authorization added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :current_user
+
+  def require_login
+    redirect_to new_sessions_path if session[:user_id] == nil
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_login, except: [:new, :create]
+
   def new
   end
 
@@ -10,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user
     else
       flash[:errors] = @user.errors.full_messages
